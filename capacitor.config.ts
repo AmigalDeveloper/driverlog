8eca8e719e431ac33469d76bd545e30740da6e28
AmigalDeveloper <developer@amigal.dk>
2022-12-18 22:57:33 +0100

    update config from test-driver-log to driver-log

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'test-driver-log',
+  appName: 'driver-log',
   webDir: 'www',
   bundledWebRuntime: false
 };
