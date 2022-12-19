6f22efd78327f56993a99294a7d3e6b11dee50ec
AmigalDeveloper <developer@amigal.dk>
2022-12-19 07:38:42 +0100

    modified:   src/app/logEntries/list-log-entries/list-log-entries.component.html 	modified:   src/app/logEntries/log-entry.ts 	modified:   tsconfig.json

--- src/app/logEntries/log-entry.ts
+++ src/app/logEntries/log-entry.ts
@@ -1,4 +1,5 @@
 export class LogEntry {
+
   logEntryDetail!: {
     date: Date;
     distanceStart: number;
@@ -11,47 +12,108 @@ export class LogEntry {
     amount: number;
     amountUnit: string;
     priceUnit: number;
+    distance: number;
   };
 
-  constructor(date?, distanceStart?, distanceEnd?, description?, fuel?, amount?, amountUnit?, priceUnit?) {
-    this.setLogEntryDetail({date: new Date(),distanceStart: 0, distanceEnd: 0,description: ' start', fuel: true});
-    this.setFuelEntryDetail({amount:0,amountUnit: 'liter', priceUnit: 0});
+  constructor(
+    date?: Date,
+    distanceStart?: number,
+    distanceEnd?: number,
+    description?: string,
+    fuel?: boolean,
+    amount?: number,
+    amountUnit?: string,
+    priceUnit?: number,
+    distance?: number
+  ) {
+    this.setLogEntryDetail({
+      date: new Date(),
+      distanceStart: 0,
+      distanceEnd: 0,
+      description: ' start',
+      fuel: true,
+    });
+    this.setFuelEntryDetail({
+      amount: 0,
+      amountUnit: 'liter',
+      priceUnit: 0,
+      distance: 0,
+    });
     console.log(
       'constructor input',
-      'date:',date,
-      'distanceStart:', distanceStart,
-      'distanceEnd:', distanceEnd,
-      'description:', description,
-      'fuel:', fuel,
-      'amount:', amount,
-      'amountUnit:', amountUnit,
-      'priceUnit:', priceUnit
-      );
+      'date:',
+      date,
+      'distanceStart:',
+      distanceStart,
+      'distanceEnd:',
+      distanceEnd,
+      'description:',
+      description,
+      'fuel:',
+      fuel,
+      'amount:',
+      amount,
+      'amountUnit:',
+      amountUnit,
+      'priceUnit:',
+      priceUnit,
+      'distance:',
+      distance
+    );
 
-    if(date) {this.logEntryDetail.date = date;}
-    if(distanceStart) { this.logEntryDetail.distanceStart = distanceStart;}
-    if(distanceEnd) { this.logEntryDetail.distanceEnd = distanceEnd;}
-    if(description) { this.logEntryDetail.description = description;}
-    if(fuel) { this.logEntryDetail.fuel = fuel;}
-    if(amount) { this.fuelEntryDetail.amount = amount; }
-    if(amountUnit) { this.fuelEntryDetail.amountUnit = amountUnit;}
-    if(priceUnit) { this.fuelEntryDetail.priceUnit = priceUnit;}
+    if (date) {
+      this.logEntryDetail.date = date;
+    }
+    if (distanceStart) {
+      this.logEntryDetail.distanceStart = distanceStart;
+    }
+    if (distanceEnd) {
+      this.logEntryDetail.distanceEnd = distanceEnd;
+    }
+    if (description) {
+      this.logEntryDetail.description = description;
+    }
+    if (fuel) {
+      this.logEntryDetail.fuel = fuel;
+    }
+    if (amount) {
+      this.fuelEntryDetail.amount = amount;
+    }
+    if (amountUnit) {
+      this.fuelEntryDetail.amountUnit = amountUnit;
+    }
+    if (priceUnit) {
+      this.fuelEntryDetail.priceUnit = priceUnit;
+    }
+    if (distance) {
+      this.fuelEntryDetail.distance = distance;
+    }
   }
 
-  getLogEntryDetail() {
+  get LogEntryDetail() {
     return this.logEntryDetail;
   }
 
-  getFuelEntryDetail() {
+  get FuelEntryDetail() {
     return this.fuelEntryDetail;
   }
 
-  setLogEntryDetail(value){
+  setLogEntryDetail(value: {
+    date: Date;
+    distanceStart: number;
+    distanceEnd: number;
+    description: string;
+    fuel: boolean;
+  }) {
     this.logEntryDetail = value;
   }
 
-  setFuelEntryDetail(value){
+  setFuelEntryDetail(value: {
+    amount: number;
+    amountUnit: string;
+    priceUnit: number;
+    distance: number;
+  }) {
     this.fuelEntryDetail = value;
   }
-
 }
