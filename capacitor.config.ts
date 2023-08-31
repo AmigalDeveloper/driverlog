d2a45e52ce544e0d3fa308c1707e369843d9869c
AmigalDeveloper <developer@amigal.dk>
2023-08-31 14:12:49 +0200

    Rettelse af workflow sam sletning aindex.htm fra rod

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'driver-log',
+  appName: 'driverlog',
   webDir: 'www',
   bundledWebRuntime: false
 };
